5bca62227beea9b1f0b7808901232896769e82a6
doardo9x <eduardo010304@gmail.com>
2024-05-26 10:54:02 -0300

    Projeto simples funcionando.

--- SQL - Necessário executar no WorkBench.sql
+++ SQL - Necessário executar no WorkBench.sql
@@ -1,20 +1,10 @@
 CREATE DATABASE liduca;
 USE liduca;
 
-CREATE TABLE generos (
-    id_genero INT AUTO_INCREMENT PRIMARY KEY,
-    nome_genero VARCHAR(255) NOT NULL
-);
-
 CREATE TABLE jogos (
     id_jogo INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
+    nome VARCHAR(255) NOT NULL unique,
     lancamento VARCHAR(255) NOT NULL,
-    desenvolvedor VARCHAR(50),
-    id_genero INT,
-    FOREIGN KEY (id_genero) REFERENCES generos(id_genero)
+    genero VARCHAR(255) NOT NULL
 );
 
--- INSERT INTO generos (nome_genero) VALUES
-
--- INSERT INTO jogos (nome, lancamento, id_genero) VALUES
